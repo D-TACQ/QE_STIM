1a72354c1d1f1ecebea3e0b26f7c9ce0c23bb7fd
peter.milne@d-tacq.co.uk <peter.milne@d-tacq.co.uk>
2022-08-28 21:26:34 +0100

    Intro QuadEncoder class , dio states  On branch main 	modified:   anstostim.cpp 	modified:   anstostim.dat

--- anstostim.cpp
+++ anstostim.cpp
@@ -15,7 +15,7 @@
 #define T_RAMP      (MAX_RPS/ACC)
 #define S_RAMP      ((ACC*T_RAMP*T_RAMP)/2)
 
-#define TPS			10000000	// Ticks Per Second minimum unit of time
+#define TPS			10000000	// Ticks Per Second minimum unit of time 10MHz
 
 class Move {
 public:
@@ -24,6 +24,8 @@ public:
 	const bool forwards;
 	const unsigned long distance;
 	const long start;
+
+	static unsigned long total_distance;
 
 	Move(bool _fwds, unsigned long _dist, unsigned long _start):
 		forwards(_fwds), distance(_dist), start(_start) {}
@@ -37,10 +39,12 @@ public:
 			case '+':
 				moves.emplace_back(Move(true, dist, seq_start));
 				seq_start += dist;
+				total_distance += dist;
 				return true;
 			case '-':
 				moves.emplace_back(Move(false, dist, seq_start));
 				seq_start -= dist;
+				total_distance += dist;
 				return true;
 			default:
 				;
@@ -55,22 +59,26 @@ public:
 };
 std::vector<Move> Move::moves;
 long Move::seq_start;
+unsigned long Move::total_distance;
 
 struct Trajectory {
-	unsigned uu;		// start speed
-	unsigned vv; 		// end speed
-	unsigned tt;        // duration in ticks
+	unsigned uu;		// start speed RPS
+	unsigned vv; 		// end speed RPS
+	unsigned tt;        // duration
+	static unsigned ttotal;
 	Trajectory(unsigned _uu, unsigned _vv, unsigned _tt):
-		uu(_uu), vv(_vv), tt(_tt)
-	{}
+		uu(_uu), vv(_vv), tt(_tt) {
+		ttotal += tt;
+		//printf("Trajectory tt:%u ttotal:%u\n", tt, ttotal);
+	}
 	void print() const {
 		printf("Trajectory: uu:%3u vv:%3u tt:%3u  => ss:%u\n", uu, vv, tt, (uu+vv)*tt/2);
 	}
 };
 
+unsigned Trajectory::ttotal;
+
 class Motion {
-	std::vector<Trajectory> stages;
-	const Move& move;
 
 	Motion(const Move& _move): move(_move) {
 		if (move.distance > 2*S_RAMP){
@@ -86,6 +94,9 @@ class Motion {
 		}
 	}
 public:
+	std::vector<Trajectory> stages;
+	const Move& move;
+
 	void print() const {
 		printf("Motion of Move: "); move.print();
 		for (auto s: stages){
@@ -100,6 +111,45 @@ public:
 };
 
 std::vector<Motion> Motion::motions;
+
+class QuadEncoder {
+	const int ABIT;
+	const int BBIT;
+	const int IBIT;
+	const int EBIT;
+
+	unsigned long long tick;
+	unsigned char* dio;				// only 4 bits are valid;
+
+	void speed_up(const Trajectory& tj) {
+		printf("speed_up\n");
+	}
+	void full_ahead(const Trajectory& tj){
+		printf("full_ahead\n");
+	}
+	void slow_down(const Trajectory& tj){
+		printf("slow down\n");
+	}
+public:
+	QuadEncoder(): ABIT(1<<0), BBIT(1<<1), IBIT(1<<2), EBIT(1<<3), tick(0) {
+		dio = new unsigned char[Trajectory::ttotal*TPS];
+		printf("QuadEncoder: tt:%u s TPS:%u  states:%u\n",
+				Trajectory::ttotal, TPS, Trajectory::ttotal*TPS);
+	}
+
+	void operator() (const Motion& motion) {
+		printf("QuadEncoder:"); motion.print();
+		for (const Trajectory tj: motion.stages){
+			if (tj.uu < tj.vv){
+				speed_up(tj);
+			}else if (tj.uu > tj.vv){
+				slow_down(tj);
+			}else{
+				full_ahead(tj);
+			}
+		}
+	}
+};
 
 int main(int argc, const char* argv[]){
 	printf("anstostim\n");
@@ -120,7 +170,9 @@ int main(int argc, const char* argv[]){
 		m.print();
 		Motion::add(m);
 	}
+	printf("Total distance: %lu\n", Move::total_distance);
+	QuadEncoder qe;
 	for (const Motion& m : Motion::motions){
-		m.print();
+		qe(m);
 	}
 }
